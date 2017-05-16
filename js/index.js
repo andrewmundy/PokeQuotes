b29d80289af433d2fdd308860a16943bf24840dd
Andrew Mundy <andrewmundy@gmail.com>
2017-05-16 01:43:00 -0700

    db integration

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
 $(document).ready(function() {
     function thing() {
-        var rand = Math.ceil(Math.random() * 360);
+        let random = num => Math.ceil((Math.random() * num) -1);
 
-        $.getJSON("https://pokeapi.co/api/v2/pokemon/" + rand).then(function(data) {
-            var sprite = data.sprites.front_default;
-            var back = data.sprites.back_default;
-            var quote = data.species.name;
-            var name = data.species.name;
-            var r = Math.floor(Math.random() * 3);
+        $.getJSON("https://pokeapi.co/api/v2/pokemon/" + random(360)).then(function(data) => {
+            let sprite = data.sprites.front_default;
+            let back = data.sprites.back_default;
+            let quote = data.species.name;
+            let name = data.species.name;
+            let r = random(3);
 
-          function getQuote(){
-            $.getJSON("https://api.mlab.com/api/1/databases/jackhandeyapi/collections/quotes?apiKey=gViXTt2ltpcF0a-Ot-Glb5w577mRXb0p").then(function(data){
-              var dataStr = data[0].jackhandey
-              var length = dataStr.length;
-              var quote = dataStr[Math.floor(Math.random()*length)].quote;
+          const getQuote() => {
+            $.getJSON("https://api.mlab.com/api/1/databases/jackhandeyapi/collections/quotes?apiKey=gViXTt2ltpcF0a-Ot-Glb5w577mRXb0p").then(function(data) => {
+              let dataStr = data[0].jackhandey
+              let length = dataStr.length;
+              let quote = dataStr[Math.floor(Math.random()*length)].quote;
               return quote;
-            });
+            };
           }
 
             function randomQuote() {
-                var r = Math.floor(Math.random() * 4);
-                var bigSlice = name.slice(0, r + 2);
-                var backSlice = name.slice(2);
-                var frontSlice = name.slice(-r + 2);
-                var randQuote = [String(backSlice), String(frontSlice), String(bigSlice), ""];
+                let r = Math.floor(Math.random() * 4);
+                let bigSlice = name.slice(0, r + 2);
+                let backSlice = name.slice(2);
+                let frontSlice = name.slice(-r + 2);
+                let randQuote = [String(backSlice), String(frontSlice), String(bigSlice), ""];
                 return randQuote[r];
             }
-            var punk = ["!", "?", "!?", "!!!", "."];
-            var quoteQuote = `"${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()}${punk[r]}"`;
+            let punk = ["!", "?", "!?", "!!!", "."];
+            let quoteQuote = `"${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()} ${randomQuote()}${punk[r]}"`;
 
-            var fullQuote = quoteQuote.toUpperCase();
-            var titleName = name[0].toUpperCase() + name.slice(1);
+            let fullQuote = quoteQuote.toUpperCase();
+            let titleName = name[0].toUpperCase() + name.slice(1);
             $(".message").empty().append(`<i>${fullQuote}</i><p>`);
             $(".message").append(`- <a href="http://www.pokemon.com/us/pokedex/${name}">${titleName}`);
             $("#zoom").empty().append(`<img class="img-responsive center-block greyscale" src="${sprite}"></a>`);
@@ -38,9 +38,9 @@ $(document).ready(function() {
 
             $("#img").one("click", function() {
               $.getJSON("https://api.mlab.com/api/1/databases/jackhandeyapi/collections/quotes?apiKey=gViXTt2ltpcF0a-Ot-Glb5w577mRXb0p").then(function(data){
-                var dataStr = data[0].jackhandey
-                var length = dataStr.length;
-                var quote = dataStr[Math.floor(Math.random()*length)].quote.slice(0,-2);
+                let dataStr = data[0].jackhandey
+                let length = dataStr.length;
+                let quote = dataStr[Math.floor(Math.random()*length)].quote.slice(0,-2);
 
                 $(".message").empty().append(`"${quote}"`);
                 $(".message").append(`<p>- <a href="http://www.pokemon.com/us/pokedex/${name}"class="less">${titleName}</p>`);
